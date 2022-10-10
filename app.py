d8e65a89103e97b23659b26d5e1063b9bf9da12d
Janitha Dassanayake <53893019+janithaDassanayake@users.noreply.github.com>
2022-10-10 22:51:07 +0530

    3rd commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import nltk
 from nltk.corpus import stopwords
+import pickle
 
 nltk.download('stopwords')
 
@@ -17,6 +18,12 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
+    # pickle.dump(Distant_KNN_Best, open("models/emo_KNN_Distant_model.pickle", 'wb'))
+    # pickle.dump(new_vectorizer, open("models/emo_knn_vectorizer.pickle", "wb"))
+
+    vectorizer = pickle.load(open("models/emo_knn_vectorizer.pickle", 'rb'))
+    model = pickle.load(open("models/emo_KNN_Distant_model.pickle", 'rb'))
+
     stop_words = stopwords.words('english')
 
     # convert to lowercase
@@ -28,7 +35,6 @@ def my_form_post():
 
     sa = SentimentIntensityAnalyzer()
     sentiment_dict = sa.polarity_scores(text=processed_doc1)
-    print(sentiment_dict['compound'])
     compound = round((1 + sentiment_dict['compound']) / 2, 2)
 
     text = text_final
@@ -38,22 +44,39 @@ def my_form_post():
 
     # decide sentiment as positive, negative and neutral
     if sentiment_dict['compound'] >= 0.05:
+        emo_status = model.predict(vectorizer.transform([processed_doc1]))[0]
         sentiment_status = "Positive"
-        print("Positive")
+        print(sentiment_status)
+        print(emo_status)
+        print("s")
 
     elif sentiment_dict['compound'] <= - 0.05:
+        emo_status = model.predict(vectorizer.transform([processed_doc1]))[0]
         sentiment_status = "Negative"
-        print("Negative")
+        print(sentiment_status)
+        print(emo_status)
+        print("N")
 
     else:
         sentiment_status = "Neutral"
-        print("Neutral")
+        emo_status = "Neutral"
+        print(sentiment_status)
+        print(emo_status)
+        print("o")
 
-    send_to_frontend = [text,positive_score,negative_score,neutral_score,sentiment_status]
+    send_to_frontend = [text, positive_score, negative_score, neutral_score, sentiment_status, emo_status]
 
-    return render_template('form.html', final=compound, text1=text_final, text2=sentiment_dict['pos'],
+    # text = typed text by user
+    # sentiment_status = overall sentiment (it will give 2 outputs Positive and negative)
+    # emo_status = text emotion status , (it will give emotion result)
+
+    final_output = "The comment you raised is considered as a  positive " + sentiment_status + "content. You have " \
+                                                                                               "commented it in a " + \
+                   emo_status + " mood.You can  proceed or update the content. "
+
+    return render_template('form.html', final=compound, text1=final_output, text2=sentiment_dict['pos'],
                            text5=sentiment_dict['neg'],
-                           text4=compound, text3=sentiment_dict['neu'])
+                           text3=sentiment_dict['neu'],text6=text)
 
 
 if __name__ == "__main__":
