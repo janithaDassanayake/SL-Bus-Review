574d7b80f953c4ff746057c61ed323761724932c
Janitha Dassanayake <53893019+janithaDassanayake@users.noreply.github.com>
2022-10-11 02:52:53 +0530

    4 commit

--- app.py
+++ app.py
@@ -18,19 +18,14 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    # pickle.dump(Distant_KNN_Best, open("models/emo_KNN_Distant_model.pickle", 'wb'))
-    # pickle.dump(new_vectorizer, open("models/emo_knn_vectorizer.pickle", "wb"))
-
     vectorizer = pickle.load(open("models/emo_knn_vectorizer.pickle", 'rb'))
     model = pickle.load(open("models/emo_KNN_Distant_model.pickle", 'rb'))
 
     stop_words = stopwords.words('english')
-
     # convert to lowercase
     text1 = request.form['text1'].lower()
-
     text_final = ''.join(c for c in text1 if not c.isdigit())
-
+    # remove stop words
     processed_doc1 = ' '.join([word for word in text_final.split() if word not in stop_words])
 
     sa = SentimentIntensityAnalyzer()
@@ -46,23 +41,14 @@ def my_form_post():
     if sentiment_dict['compound'] >= 0.05:
         emo_status = model.predict(vectorizer.transform([processed_doc1]))[0]
         sentiment_status = "Positive"
-        print(sentiment_status)
-        print(emo_status)
-        print("s")
 
     elif sentiment_dict['compound'] <= - 0.05:
         emo_status = model.predict(vectorizer.transform([processed_doc1]))[0]
         sentiment_status = "Negative"
-        print(sentiment_status)
-        print(emo_status)
-        print("N")
 
     else:
         sentiment_status = "Neutral"
         emo_status = "Neutral"
-        print(sentiment_status)
-        print(emo_status)
-        print("o")
 
     send_to_frontend = [text, positive_score, negative_score, neutral_score, sentiment_status, emo_status]
 
@@ -70,9 +56,7 @@ def my_form_post():
     # sentiment_status = overall sentiment (it will give 2 outputs Positive and negative)
     # emo_status = text emotion status , (it will give emotion result)
 
-    final_output = "The comment you raised is considered as a  positive " + sentiment_status + "content. You have " \
-                                                                                               "commented it in a " + \
-                   emo_status + " mood.You can  proceed or update the content. "
+    final_output = "The comment you raised is considered as a " + sentiment_status + " content. You have commented it in a " + emo_status + " mood.You can  proceed or update the content. "
 
     return render_template('form.html', final=compound, text1=final_output, text2=sentiment_dict['pos'],
                            text5=sentiment_dict['neg'],
